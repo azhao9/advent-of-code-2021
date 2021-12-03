dd8f38f96258602f4cd0e4b2ee94ebc280326f5e
Aleck Zhao <aleck.zhao1114@gmail.com>
2021-12-02 20:59:51 -0600

    adding day 2

--- day1/main.py
+++ day1/main.py
@@ -1,22 +1,25 @@
+from os import pathconf
 from numpy import loadtxt
 
-def sonar_sweep(depths=None):
-    if not depths:
-        depths = loadtxt('day1/input.txt')
+def sonar_sweep(path: str):
+    depths = loadtxt(path)
     count = 0
     for i in range(len(depths) - 1):
         count += bool(depths[i+1] > depths[i])
     return count
 
-print(sonar_sweep())
+print('part 1')
+print(sonar_sweep('day1/test_input.txt'))
+print(sonar_sweep('day1/input.txt'))
 
-def sonar_sweep3(depths=None):
-    if not depths:
-        depths = loadtxt('day1/input.txt')
+
+def sonar_sweep3(path: str):
+    depths = loadtxt(path)
     count = 0
     for i in range(len(depths) - 3):
         count += bool(depths[i+3] > depths[i])
     return count
 
-print(sonar_sweep3())
-
+print('part 2')
+print(sonar_sweep3('day1/test_input.txt'))
+print(sonar_sweep3('day1/input.txt'))
---
+++ day2/main.py
@@ -0,0 +1,40 @@
+def get_position(path: str):
+    depth = horizontal = 0
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        input, value = line.split(' ')
+        if input == 'forward':
+            horizontal += int(value)
+        elif input == 'down':
+            depth += int(value)
+        elif input == 'up':
+            depth -= int(value)
+        
+    return depth * horizontal
+
+print('part 1')
+print(get_position('day2/test_input.txt'))
+print(get_position('day2/input.txt'))
+
+def get_position2(path: str):
+    depth = horizontal = aim = 0
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        input, value = line.split(' ')
+        if input == 'forward':
+            horizontal += int(value)
+            depth += aim * int(value)
+        elif input == 'down':
+            aim += int(value)
+        elif input == 'up':
+            aim -= int(value)
+    
+    return depth * horizontal
+
+print('part 2')
+print(get_position2('day2/test_input.txt'))
+print(get_position2('day2/input.txt'))
